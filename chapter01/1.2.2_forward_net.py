cb2fe92bae3949e660d2d2fe2f4c0cc1a3504ce1
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-02-17 21:11:37 +0900

    study 1.2.2_forward_net.py

--- chapter01/1.2.2_forward_net.py
+++ chapter01/1.2.2_forward_net.py
@@ -1,43 +1,46 @@
-# coding: utf-8
 import numpy as np
 
 
+# define sigmoid layer with class
 class Sigmoid:
+    # save parameters
     def __init__(self):
-        self.params = []
+        self.params = []  # there is no learning params, so restore it
 
+    # sigmoid function
     def forward(self, x):
         return 1 / (1 + np.exp(-x))
 
 
+# define affine layer with class
 class Affine:
+    # save parameters: W: weight, b: bias
     def __init__(self, W, b):
         self.params = [W, b]
 
+    # affine function with forward
     def forward(self, x):
         W, b = self.params
         out = np.dot(x, W) + b
         return out
 
 
+# define affine - sigmoid - affine layers with class
 class TwoLayerNet:
+    # reset params and create 3 layers
     def __init__(self, input_size, hidden_size, output_size):
         I, H, O = input_size, hidden_size, output_size
 
-        # 가중치와 편향 초기화
-        W1 = np.random.randn(I, H)
+        # reset weight and bias
+        W1 = np.random.randn(I, H)  # for now, these params are random
         b1 = np.random.randn(H)
         W2 = np.random.randn(H, O)
         b2 = np.random.randn(O)
 
-        # 계층 생성
-        self.layers = [
-            Affine(W1, b1),
-            Sigmoid(),
-            Affine(W2, b2)
-        ]
+        # create layers
+        self.layers = [Affine(W1, b1), Sigmoid(), Affine(W2, b2)]
 
-        # 모든 가중치를 리스트에 모은다.
+        # collect all params
         self.params = []
         for layer in self.layers:
             self.params += layer.params
@@ -48,7 +51,7 @@ class TwoLayerNet:
         return x
 
 
-x = np.random.randn(10, 2)
-model = TwoLayerNet(2, 4, 3)
-s = model.predict(x)
+x = np.random.randn(10, 2)  # input data
+model = TwoLayerNet(2, 4, 3)  # model
+s = model.predict(x)  # predict result
 print(s)
