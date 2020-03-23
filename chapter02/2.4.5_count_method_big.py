6be75928611a0c4eecb39014d9e3a3378bb3101f
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-03-23 20:14:26 +0900

    study 2.4.5_count_method_big.py

--- chapter02/2.4.5_count_method_big.py
+++ chapter02/2.4.5_count_method_big.py
@@ -1,29 +1,25 @@
-# coding: utf-8
-import sys
-sys.path.append('..')
+from commons.util import most_similar, create_co_matrix, ppmi
+from datasets import ptb
 import numpy as np
-from common.util import most_similar, create_co_matrix, ppmi
-from dataset import ptb
-
 
 window_size = 2
 wordvec_size = 100
 
 corpus, word_to_id, id_to_word = ptb.load_data('train')
 vocab_size = len(word_to_id)
-print('동시발생 수 계산 ...')
+print('Calculating coincide number ...')
 C = create_co_matrix(corpus, vocab_size, window_size)
-print('PPMI 계산 ...')
+print('Calculating PPMI ...')
 W = ppmi(C, verbose=True)
 
-print('calculating SVD ...')
+print('Calculating SVD ...')
 try:
-    # truncated SVD (빠르다!)
+    # truncated SVD(fast)
     from sklearn.utils.extmath import randomized_svd
-    U, S, V = randomized_svd(W, n_components=wordvec_size, n_iter=5,
-                             random_state=None)
+    U, S, V = randomized_svd(
+        W, n_components=wordvec_size, n_iter=5, random_state=None)
 except ImportError:
-    # SVD (느리다)
+    # SVD(slow)
     U, S, V = np.linalg.svd(W)
 
 word_vecs = U[:, :wordvec_size]
