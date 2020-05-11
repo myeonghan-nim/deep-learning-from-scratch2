265af7293930286fdae3db75c8dd7e066af4d39f
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-05-11 18:21:18 +0900

    study 5.5.3_train_custom_loop.py

--- chapter05/5.5.3_train_custom_loop.py
+++ chapter05/5.5.3_train_custom_loop.py
@@ -1,50 +1,46 @@
-# coding: utf-8
-import sys
-sys.path.append('..')
+from commons.optimizer import SGD
+from datasets import ptb
+from simple_rnnlm import SimpleRnnlm
 import matplotlib.pyplot as plt
 import numpy as np
-from common.optimizer import SGD
-from dataset import ptb
-from simple_rnnlm import SimpleRnnlm
 
-
-# 하이퍼파라미터 설정
+# set hyperparameters
 batch_size = 10
 wordvec_size = 100
-hidden_size = 100 # RNN의 은닉 상태 벡터의 원소 수
-time_size = 5     # Truncated BPTT가 한 번에 펼치는 시간 크기
+hidden_size = 100  # number of hidden vectors in RNN
+time_size = 5  # spread time area at Truncated BPTT once
 lr = 0.1
 max_epoch = 100
 
-# 학습 데이터 읽기(전체 중 1000개만)
+# read datasets(first 1000)
 corpus, word_to_id, id_to_word = ptb.load_data('train')
 corpus_size = 1000
 corpus = corpus[:corpus_size]
 vocab_size = int(max(corpus) + 1)
 
-xs = corpus[:-1]  # 입력
-ts = corpus[1:]   # 출력(정답 레이블)
+xs = corpus[:-1]  # input
+ts = corpus[1:]  # output(answer)
 data_size = len(xs)
-print('말뭉치 크기: %d, 어휘 수: %d' % (corpus_size, vocab_size))
+print(f'Corpus Size: {corpus_size}, Number of Vocab: {vocab_size}')
 
-# 학습 시 사용하는 변수
+# variables for learning
 max_iters = data_size // (batch_size * time_size)
 time_idx = 0
 total_loss = 0
 loss_count = 0
 ppl_list = []
 
-# 모델 생성
+# init model
 model = SimpleRnnlm(vocab_size, wordvec_size, hidden_size)
 optimizer = SGD(lr)
 
-# 미니배치의 각 샘플의 읽기 시작 위치를 계산
+# calculate start reading point of each sample of mini batch
 jump = (corpus_size - 1) // batch_size
 offsets = [i * jump for i in range(batch_size)]
 
 for epoch in range(max_epoch):
     for iter in range(max_iters):
-        # 미니배치 취득
+        # get mini batch
         batch_x = np.empty((batch_size, time_size), dtype='i')
         batch_t = np.empty((batch_size, time_size), dtype='i')
         for t in range(time_size):
@@ -53,21 +49,20 @@ for epoch in range(max_epoch):
                 batch_t[i, t] = ts[(offset + time_idx) % data_size]
             time_idx += 1
 
-        # 기울기를 구하여 매개변수 갱신
+        # renewal params with grads
         loss = model.forward(batch_x, batch_t)
         model.backward()
         optimizer.update(model.params, model.grads)
         total_loss += loss
         loss_count += 1
 
-    # 에폭마다 퍼플렉서티 평가
+    # evalueate perplexity of each epoch
     ppl = np.exp(total_loss / loss_count)
-    print('| 에폭 %d | 퍼플렉서티 %.2f'
-          % (epoch+1, ppl))
+    print(f'| Epoch {epoch + 1} | Perplexity {ppl:.2f}')
     ppl_list.append(float(ppl))
     total_loss, loss_count = 0, 0
 
-# 그래프 그리기
+# draw graph
 x = np.arange(len(ppl_list))
 plt.plot(x, ppl_list, label='train')
 plt.xlabel('epochs')
